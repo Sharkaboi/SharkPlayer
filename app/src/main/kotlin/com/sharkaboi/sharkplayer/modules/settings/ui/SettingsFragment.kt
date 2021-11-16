f14f5520025de62dd4ae91260d3afa08296f519d
Sarath S <sarathsedu@gmail.com>
2021-11-16 11:10:59 +0530

    Remove settings options

--- app/src/main/kotlin/com/sharkaboi/sharkplayer/modules/settings/ui/SettingsFragment.kt
+++ app/src/main/kotlin/com/sharkaboi/sharkplayer/modules/settings/ui/SettingsFragment.kt
@@ -7,15 +7,9 @@ import androidx.fragment.app.viewModels
 import androidx.preference.Preference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreference
-import com.sharkaboi.appupdatechecker.models.UpdateState
 import com.sharkaboi.sharkplayer.BuildConfig
 import com.sharkaboi.sharkplayer.R
-import com.sharkaboi.sharkplayer.common.extensions.observe
-import com.sharkaboi.sharkplayer.common.extensions.openUrl
-import com.sharkaboi.sharkplayer.common.extensions.showOneOpDialog
-import com.sharkaboi.sharkplayer.common.extensions.showToast
 import com.sharkaboi.sharkplayer.data.sharedpref.SharedPrefKeys
-import com.sharkaboi.sharkplayer.modules.settings.vm.SettingsState
 import com.sharkaboi.sharkplayer.modules.settings.vm.SettingsViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -35,22 +29,6 @@ class SettingsFragment : PreferenceFragmentCompat() {
     }
 
     private fun setupObservers() {
-        observe(settingsViewModel.uiState) { state ->
-            when (state) {
-                is SettingsState.Failure -> showToast(state.message)
-                is SettingsState.Success -> showUpdateAvailableDialog(state.message)
-                else -> Unit
-            }
-        }
-    }
-
-    private fun showUpdateAvailableDialog(updateState: UpdateState.UpdateAvailable) {
-        context?.showOneOpDialog(
-            title = getString(R.string.update_dialog_title, updateState.latestVersion),
-            message = updateState.releaseNotes,
-            buttonHintId = R.string.open,
-            onClick = { openUrl(updateState.latestVersionUrl) }
-        )
     }
 
     private fun setupListeners() {
@@ -58,10 +36,6 @@ class SettingsFragment : PreferenceFragmentCompat() {
             Preference.SummaryProvider { _: Preference ->
                 getString(R.string.version, BuildConfig.VERSION_NAME)
             }
-        findPreference<Preference>(SharedPrefKeys.UPDATES)?.setOnPreferenceClickListener {
-            settingsViewModel.checkUpdate()
-            true
-        }
         findPreference<SwitchPreference>(SharedPrefKeys.DARK_THEME)?.setOnPreferenceChangeListener { _, newValue ->
             AppCompatDelegate.setDefaultNightMode(
                 if (newValue == true)
